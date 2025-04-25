0c557cfeb34eb71e071c7d7665107f00602e4370
Florian Uhlig <f.uhlig@gsi.de>
2025-04-25 10:50:15 +0200

    [legacy] root: Bump 6.36.00-rc1
    Fix also a problem when building the vecgeom interface.

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -371,7 +371,7 @@ ExternalProject_Add(geant4
 )
 
 list(APPEND packages root)
-set(root_version "6.34.08")
+set(root_version "6.36.00-rc1")
 string(REPLACE "\." "-" root_version_gittag ${root_version})
 if(APPLE AND CMAKE_VERSION VERSION_GREATER 3.15)
   set(root_builtin_glew "-Dbuiltin_glew=ON")
@@ -429,7 +429,7 @@ ExternalProject_Add(root
     ${root_cocoa}
   UPDATE_DISCONNECTED ON
   PATCH_COMMAND ${patch} -p1 -i "${CMAKE_SOURCE_DIR}/legacy/root/fix_macos_sdk_mismatch.patch"
-  COMMAND ${patch} -p1 -i "${CMAKE_SOURCE_DIR}/legacy/root/vector-TObject-v6-34.patch"
+  COMMAND ${patch} -p1 -i "${CMAKE_SOURCE_DIR}/legacy/root/fix_vecgeom.patch"
   DEPENDS pythia8 vc vecgeom ${extract_source_cache_target}
   ${LOG_TO_FILE}
 )
