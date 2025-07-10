ae59a3f1279583508f0a6340872ee640c0c72117
Florian Uhlig <f.uhlig@gsi.de>
2025-07-10 16:17:11 +0200

    [legacy] root: Bump 6.36.02

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -371,7 +371,7 @@ ExternalProject_Add(geant4
 )
 
 list(APPEND packages root)
-set(root_version "6.36.00-rc1")
+set(root_version "6.36.02")
 string(REPLACE "\." "-" root_version_gittag ${root_version})
 if(APPLE AND CMAKE_VERSION VERSION_GREATER 3.15)
   set(root_builtin_glew "-Dbuiltin_glew=ON")
